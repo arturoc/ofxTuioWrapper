caa09e5e39a09ca0a5109582049743ec860985df
arturo <arturo@openframeworks.cc>
2010-01-06 19:55:31 +0100

    first import

---
+++ example/src/testApp.cpp
@@ -0,0 +1,91 @@
+#include "testApp.h"
+
+//--------------------------------------------------------------
+void testApp::setup(){
+	ofBackground(200, 200, 200);
+	ofSetVerticalSync(true);
+	
+	//Connect to Port
+	myTuio.connect(3333);
+	
+	
+	//Assign Global TUIO Callback Functions
+	ofAddListener(ofEvents.touchDown,this,&testApp::touchDown);
+	ofAddListener(ofEvents.touchUp,this,&testApp::touchUp);
+	ofAddListener(ofEvents.touchMoved,this,&testApp::touchMoved);
+
+}
+
+//--------------------------------------------------------------
+void testApp::update(){
+
+}
+
+//--------------------------------------------------------------
+void testApp::draw(){
+	
+	//render TUIO Cursors and Objects
+	myTuio.drawCursors();
+	myTuio.drawObjects();
+}
+
+//--------------------------------------------------------------
+void testApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void testApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::windowResized(int w, int h){
+
+}
+
+
+void testApp::touchDown(ofTouchEventArgs & touch){
+	cout << " cursor added: " + ofToString(touch.id)+
+	" X: "+ofToString(touch.x)+
+	" Y: "+ofToString(touch.y)
+	<< endl;
+
+	
+}
+
+void testApp::touchUp(ofTouchEventArgs & touch){
+	cout << " cursor removed: " + ofToString(touch.id)+
+	 " X: "+ofToString(touch.x)+
+	 " Y: "+ofToString(touch.y)
+	 << endl;
+	
+}
+
+void testApp::touchMoved(ofTouchEventArgs & touch){
+	cout << " cursor updated: " + ofToString(touch.id)+
+	 " X: "+ofToString(touch.x)+
+	 " Y: "+ofToString(touch.y)
+	 << endl;
+}
+
